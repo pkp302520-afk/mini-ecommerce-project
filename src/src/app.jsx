6d6f312b3e34078ab09450be5dff5ca107fcded6
Balamurali <pkp302520@gmail.com>
2026-06-24 14:49:50 +0530

    Remove duplicate app.jsx

--- src/src/app.jsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1 style={{ color: "blue" }}>
-        Mini E-Commerce Working ✅
-      </h1>
-    </div>
-  );
-}
